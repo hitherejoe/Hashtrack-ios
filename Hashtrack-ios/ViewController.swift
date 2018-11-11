5842ceb53682e11ee56c81a48452c6792300873b
Joe Birch <hitherejoebirch@googlemail.com>
2018-11-11 12:47:58 +0000

    Initial Commit

---
+++ Hashtrack-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hashtrack-ios
+//
+//  Created by Joe Birch on 11/11/2018.
+//  Copyright © 2018 hitherejoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Hashtrack-iosTests/Hashtrack_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  Hashtrack_iosTests.swift
+//  Hashtrack-iosTests
+//
+//  Created by Joe Birch on 11/11/2018.
+//  Copyright © 2018 hitherejoe. All rights reserved.
+//
+
+import XCTest
+@testable import Hashtrack_ios
+
+class Hashtrack_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
